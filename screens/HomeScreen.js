37fd8e511063351e74032f49c3e7eb8baf21bcf4
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2023-04-11 22:09:53 -0700

    week 4

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,51 +1,86 @@
-import { Text, View, ScrollView } from 'react-native';
-import { useState } from 'react'
-import { Card } from 'react-native-elements'
-import { CAMPSITES } from '../shared/CAMPSITES';
-import { PROMOTIONS } from '../shared/promotions';
-import { PARTNERS } from '../shared/partners';
+import { useEffect, useRef } from 'react';
+import { Text, View, Animated } from 'react-native';
+import { Card } from 'react-native-elements';
+import { useSelector } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponent';
 
-const FeaturedItem = ({item}) => {
-    if(item) {
+const FeaturedItem = (props) => {
+    const { item } = props;
+
+    if (props.isLoading) {
+        return <Loading />;
+    }
+    if (props.errMess) {
         return (
-            <Card containerStyle={{padding: 0}}>
-                <Card.Image source={item.image}>
-                    <View style={{justifyContent: 'center', flex: 1}}>
-                        <Text style={{
-                            color: 'white', 
-                            textAlign: 'center', 
-                            fontSize: 20
+            <View>
+                <Text>{props.errMess}</Text>
+            </View>
+        );
+    }
+    if (item) {
+        return (
+            <Card containerStyle={{ padding: 0 }}>
+                <Card.Image source={{ uri: baseUrl + item.image }}>
+                    <View style={{ justifyContent: 'center', flex: 1 }}>
+                        <Text
+                            style={{
+                                color: 'white',
+                                textAlign: 'center',
+                                fontSize: 20
                             }}
                         >
                             {item.name}
                         </Text>
                     </View>
                 </Card.Image>
-                <Text style={{margin: 20}}>{item.description}</Text>
+                <Text style={{ margin: 20 }}>{item.description}</Text>
             </Card>
-        )
+        );
     }
-   return <View />
-}
-
+    return <View />;
+};
 
 const HomeScreen = () => {
-    const [campsites, setCampsites] = useState(CAMPSITES);
-    const [promotions, setPromotions] = useState(PROMOTIONS);
-    const [partners, setPartners] = useState(PARTNERS);
+    const campsites = useSelector((state) => state.campsites);
+    const promotions = useSelector((state) => state.promotions);
+    const partners = useSelector((state) => state.partners);
+    const scaleValue = useRef(new Animated.Value(0)).current;
+    const scaleAnimation = Animated.timing(scaleValue, {
+        toValue: 1,
+        duration: 1500,
+        useNativeDriver: true
+    });
 
-    const featCampsite = campsites.find(campsite => campsite.featured);
-    const featPromotion = promotions.find(promotions => promotions.featured)
-    const featPartner = partners.find(partners => partners.featured)
+    const featCampsite = campsites.campsitesArray.find((item) => item.featured);
+    const featPromotion = promotions.promotionsArray.find(
+        (item) => item.featured
+    );
+    const featPartner = partners.partnersArray.find((item) => item.featured);
 
+    useEffect(() => {
+        scaleAnimation.start();
+    }, []);
 
-  return (
-    <ScrollView>
-        <FeaturedItem item={featCampsite} />
-        <FeaturedItem item={featPromotion} />
-        <FeaturedItem item={featPartner} />
-    </ScrollView>
-  )
-}
+    return (
+        <Animated.ScrollView style={{ transform: [{ scale: scaleValue }] }}>
+            <FeaturedItem
+                item={featCampsite}
+                isLoading={campsites.isLoading}
+                errMess={campsites.errMess}
+            />
+            <FeaturedItem
+                item={featPromotion}
+                isLoading={promotions.isLoading}
+                errMess={promotions.errMess}
+            />
+            <FeaturedItem
+                item={featPartner}
+                isLoading={partners.isLoading}
+                errMess={partners.errMess}
+            />
+        </Animated.ScrollView>
+    );
+};
 
-export default HomeScreen
+export default HomeScreen;
